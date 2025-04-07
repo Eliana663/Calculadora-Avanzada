8b4cbef0d7b5ef9b9f07dd41b8073733ac32892c
Eliana663 <elianadtorrest@gmail.com>
2025-04-07 14:51:24 +0200

    Funciones pulsar numero y operacion

--- app/src/main/java/com/example/calculadora_Avanzada/MainActivity.kt
+++ app/src/main/java/com/example/calculadora_Avanzada/MainActivity.kt
@@ -1,16 +1,27 @@
 package com.example.calculadora_Avanzada
 
+import android.R.attr.button
+import android.icu.text.DecimalFormat
 import android.os.Bundle
 import android.view.View
-import android.widget.EditText
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import com.example.calculadora.R
 
 class MainActivity : ComponentActivity() {
 
+    var numero1 = ""
+    var numero2 = ""
+    var operador = ""
 
-    lateinit var campoNumero : EditText
-    lateinit var resultado : EditText
+
+    lateinit var operacion : TextView
+    lateinit var resultado : TextView
+
+
+     lateinit var formatoDecimal: DecimalFormat
 
 
 
@@ -18,24 +29,34 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        campoNumero = this.findViewById(R.id.Numero)
-        resultado = this.findViewById(R.id.Result)
+        formatoDecimal = DecimalFormat("#.#########")
+
+        operacion = this.findViewById(R.id.operacion)
+        resultado = this.findViewById(R.id.resultado)
+
+
+
+    }
+    // Botón para mostrar cada vez que se pulsa un número en el textview de operacion
+    fun pulsarNumero (view: View) {
+        val button:Button = view as Button;
+        numero1 = button.text.toString()
+        operacion.text = numero1
+        Toast.makeText(this,button.text,Toast.LENGTH_LONG).show();
+    }
+    // Botón para mostrar cada vez que se pulsa una operacion
+    fun pulsarOperacion (view: View) {
+        val button: Button = view as Button;
+        operador = button.text.toString()
+        operacion.text = operador
 
     }
 
-    fun sumar(view: View){
-        var campoNumero: String = campoNumero.text.toString()
-        val resultadotempo= campoNumero
-        resultado.setText(resultadotempo.toString())
+    fun ACReset(view: View) {
+        operacion.text = "0"
+        resultado.text = "0"
     }
 
-
-
-    fun reset(view: View){
-        campoNumero.setText("")
-        resultado.setText("")
-
-    }
 
 
     }
